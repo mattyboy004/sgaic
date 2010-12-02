7db496fc7d6eba75a6e8d03c8a48a485d586c170
Cesar Kawakami <cesarkawakami@gmail.com>
2010-12-02 03:31:26 -0200

    Nondaemonizing anymore.

--- genetic/client.py
+++ genetic/client.py
@@ -22,6 +22,6 @@ def execute():
 
 
 if __name__ == '__main__':
-	daemonize.daemonize(stdin='/dev/null', stdout='./log/stdout-%s-log.txt'%sys.argv[1], stderr='./log/stderr-%s-log.txt'%sys.argv[1])
+	# daemonize.daemonize(stdin='/dev/null', stdout='./log/stdout-%s-log.txt'%sys.argv[1], stderr='./log/stderr-%s-log.txt'%sys.argv[1])
 	run(host='0.0.0.0',
 	    port=int(sys.argv[1]))
